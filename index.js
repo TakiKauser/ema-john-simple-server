74c85793148236a250e5aa665ee525b5e0d9eac4
TakiKauser <takikauserbadhon@gmail.com>
2021-10-26 15:39:49 +0600

    GET-API

--- index.js
+++ index.js
@@ -16,15 +16,21 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sydng.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-console.log(uri);
-
 async function run() {
     try {
         await client.connect();
-        console.log("Connected Successfully");
+        const database = client.db('online_shop');
+        const productCollection = database.collection('products');
+
+        // GET API
+        app.get('/products', async (req, res) => {
+            const cursor = productCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        });
     }
     finally {
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
