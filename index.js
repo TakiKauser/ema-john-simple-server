d6887d6297e5543163fd4a4f61b38e99b6c8fb9c
TakiKauser <takikauserbadhon@gmail.com>
2021-10-26 15:15:32 +0600

    db-connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,6 +9,25 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// DB_USER=ema-john-simple-user
+// DB_PASS=olYY8cDmST8pOGpT
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sydng.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+console.log(uri);
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected Successfully");
+    }
+    finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send("Ema-john server is running.");
